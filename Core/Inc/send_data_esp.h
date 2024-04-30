65b1d7a1d54cf7897c59510397b8fa14bcca5d6e
mint0102 <hahuyen010102@gmail.com>
2024-04-30 16:04:26 +0700

    send data to ada

---
+++ Core/Inc/send_data_esp.h
@@ -0,0 +1,18 @@
+/*
+ * send_data_esp.h
+ *
+ *  Created on: Apr 24, 2024
+ *      Author: HaHuyen
+ */
+
+#ifndef INC_SEND_DATA_ESP_H_
+#define INC_SEND_DATA_ESP_H_
+
+#include "gpio.h"
+#include "uart.h"
+#include "button.h"
+#include "lcd.h"
+
+void sendData(uint8_t data);
+
+#endif /* INC_SEND_DATA_ESP_H_ */
---
+++ Core/Src/send_data_esp.c
@@ -0,0 +1,13 @@
+/*
+ * send_data_esp.c
+ *
+ *  Created on: Apr 24, 2024
+ *      Author: HaHuyen
+ */
+
+#include "send_data_esp.h"
+
+//void sendData(uint8_t data){
+//	uart_EspSendString(str);
+//}
+
